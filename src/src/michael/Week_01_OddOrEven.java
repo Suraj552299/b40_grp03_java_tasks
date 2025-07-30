fd4048796c255058d787bd48614f6738b6938944
Ersin <michael.knight@cydeo.com>
2025-07-31 00:09:25 +0300

    Created Week_01_OddOrEven class

---
+++ src/src/michael/Week_01_OddOrEven.java
@@ -0,0 +1,16 @@
+package src.michael;
+
+public class Week_01_OddOrEven {
+
+
+    /*
+    *  Write  a method which can identifies given number is even or odd
+EX:
+identify(5) ->  "Odd"
+identify(6) ->  "Even"
+* */
+
+    public static void main(String[] args) {
+
+    }
+}
