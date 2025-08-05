5ed2aa2e1e4a7a16af43dd8d9458fb2134ce2d50
Suraj552299 <ernazarowsuraj@gmail.com>
2025-08-05 22:27:41 +0300

    Task2 has been completed.

--- src/src/suroj/Finra.java
+++ src/src/suroj/Finra.java
@@ -4,20 +4,20 @@ public class Finra {
     public static void main(String[] args) {
 String result;
 
-        for (int number = 1; number <=30 ; number++) {
-            boolean r1=number%3==0&number%5==0;
-            boolean r2=number%3==0;
-            boolean r3=number%5==0;
-            if (r1){
+        for (int number = 1; number <= 30; number++) {
+            boolean r1 = number % 3 == 0 & number % 5 == 0;
+            boolean r2 = number % 3 == 0;
+            boolean r3 = number % 5 == 0;
+            if (r1) {
                 result = "FINRA";
             } else if (r2) {
-                result ="FIN";
+                result = "FIN";
             } else if (r3) {
-               result="RA";
-            }else{
-                result=""+number;
+                result = "RA";
+            } else {
+                result = "" + number;
             }
-            System.out.print(result+" ");
+            System.out.print(result + " ");
         }
     }
 }
