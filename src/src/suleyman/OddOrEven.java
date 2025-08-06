9b8634e559952d58f7c238cea36ad7657299fb39
Süleyman <fearlessfelix20@gmail.com>
2025-08-06 22:46:04 +0300

    OddOrEven Task is done.

--- src/src/suleyman/OddOrEven.java
+++ src/src/suleyman/OddOrEven.java
@@ -2,7 +2,22 @@ package src.suleyman;
 
 public class OddOrEven {
     public static void main(String[] args) {
-        int num=7;
+        int number=7;
+        oddOrEvenMethod(number);
+        oddOrEvenMethod(-7);
+        oddOrEvenMethod(8);
+        oddOrEvenMethod(0);
+        oddOrEvenMethod(-8);
 
+
+    }
+
+
+    public static void oddOrEvenMethod(int num){
+        if(num%2==0){
+            System.out.println(num+ " is an even number.");
+        }else {
+            System.out.println(num + " is an odd number.");
+        }
     }
 }
