ba4dac9b25d27bc22668623b6400adc92aa9c16f
tegelverenn <tegelveren@gmail.com>
2025-08-06 22:46:12 +0300

    OddOrEven task is done.

--- src/src/talha/Week_01_OddOrEven.java
+++ src/src/talha/Week_01_OddOrEven.java
@@ -3,8 +3,22 @@ package src.talha;
 public class Week_01_OddOrEven {
     public static void main(String[] args) {
 
-        int num = 7;
+        int num = 8;
+        oddOrEvenMethod(9);
+        oddOrEvenMethod(57);
+        oddOrEvenMethod(75);
+        oddOrEvenMethod(40);
 
 
+
+    }
+
+    public static void oddOrEvenMethod(int num){
+
+        if (num % 2 == 0) {
+            System.out.println(num + " is an even number");
+        } else {
+            System.out.println(num + " is an odd number");
+        }
     }
 }
