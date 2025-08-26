54c2b33b1d8faea6f826b180312c0f39811f0993
TroyYC <troy@cydeo.com>
2025-08-26 18:24:40 -0400

    new tasks added for week4 and week5

---
+++ src/src/troy/Week05_FrequencyOfCharacters.java
@@ -0,0 +1,13 @@
+package src.troy;
+
+public class Week05_FrequencyOfCharacters {
+
+    /*
+    Write function that can find the frequency of characters
+            Ex: FrequencyOfChars("AAABBCDD") ==> A3B2C1D2
+     */
+
+
+
+
+}
