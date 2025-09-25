07c72a7123f97468d491fb2f01cbcf421e0dc141
TroyYC <troy@cydeo.com>
2025-09-25 15:19:20 -0400

    week7 and week8 task added

--- src/src/troy/Week06_RemoveDuplicates.java
+++ src/src/troy/Week06_RemoveDuplicates.java
@@ -6,7 +6,23 @@ public class Week06_RemoveDuplicates {
             Ex: removeDup("AAABBBCCC") ==> ABC
      */
 
-    //Rachid is volunteer
+    //Rachid is not volunteer
+
+    public static String removeDup(String str) {
+        String result = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            if (!result.contains("" + str.charAt(i))) {
+                result += "" + str.charAt(i);//ABC
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(removeDup("AAABBBCCC"));
+    }
 
 
 
