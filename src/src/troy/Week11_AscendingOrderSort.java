afb82fafc65148ff7339842d4be620c12b0d6d33
Muyesser <muyesser@ninova.com>
2025-10-22 14:23:22 -0400

    AscendingOrder-muyesser1

---
+++ src/src/troy/Week11_AscendingOrderSort.java
@@ -0,0 +1,27 @@
+package src.troy;
+
+import java.util.Arrays;
+
+public class Week11_AscendingOrderSort {
+
+    public static void sortAscending(int[] arr) {
+        int n = arr.length;
+        // Bubble Sort
+        for (int i = 0; i < n - 1; i++) {  // this line is for every round
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // swap arr[j] and arr[j + 1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int[] numbers = {1, 5, 8, 3, 2, 11, -1};
+        sortAscending(numbers);
+        System.out.println(Arrays.toString(numbers));
+
+    }
+}
